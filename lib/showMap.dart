8223f6cfcd0bceb619bd4749416ad4e7024188e2
Enciyo <enc1y0@hotmail.com>
2018-04-26 19:09:04 +0300

    Initial commit

---
+++ lib/showMap.dart
@@ -0,0 +1,1 @@
+import 'dart:async';import 'package:url_launcher/url_launcher.dart';class showMap{   static Future<Null> launchbrowser(String long,String lati) async {    String mapURl="https://maps.google.com/maps/dir//$long,$lati/";      await launch(mapURl, forceSafariVC: false, forceWebView: false);  }}
